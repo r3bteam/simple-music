4cda5c059ff5c20ba51cf66ab84153e5055c9d9b
Abady <gamersspeaks@gmail.com>
2018-05-29 02:36:24 +0300

    he

--- music.js
+++ music.js
@@ -86,34 +86,34 @@ client.on('message', function(message) {
             message.reply("<:no:439399928960253964> you already voted to skip!");
         }
 
-     if (mess.startsWith(prefix+"search")) {
-        try {
-            var videos = await youtube.searchVideos(searchString, 10);
-            let index = 0;
-            msg.channel.send(`
-__**Song selection:**__
-${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
-Please provide a value to select one of the search results ranging from 1-10.
-            `);
-            // eslint-disable-next-line max-depth
-            try {
-                var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
-                    maxMatches: 1,
-                    time: 10000,
-                    errors: ['time']
-                });
-            } catch (err) {
-                console.error(err);
-                return msg.channel.send('No or invalid value entered, cancelling video selection.');
-            }
-            const videoIndex = parseInt(response.first().content);
-            var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
-        } catch (err) {
-            console.error(err);
-            return msg.channel.send('🆘 I could not obtain any search results.');
-        }
-    }
-    return handleVideo(video, msg, voiceChannel);
+//      if (mess.startsWith(prefix+"search")) {
+//         try {
+//             var videos = await youtube.searchVideos(searchString, 10);
+//             let index = 0;
+//             msg.channel.send(`
+// __**Song selection:**__
+// ${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
+// Please provide a value to select one of the search results ranging from 1-10.
+//             `);
+//             // eslint-disable-next-line max-depth
+//             try {
+//                 var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+//                     maxMatches: 1,
+//                     time: 10000,
+//                     errors: ['time']
+//                 });
+//             } catch (err) {
+//                 console.error(err);
+//                 return msg.channel.send('No or invalid value entered, cancelling video selection.');
+//             }
+//             const videoIndex = parseInt(response.first().content);
+//             var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+//         } catch (err) {
+//             console.error(err);
+//             return msg.channel.send('🆘 I could not obtain any search results.');
+//         }
+//     }
+//     return handleVideo(video, msg, voiceChannel);
 
     } else if (mess.startsWith(prefix + "queue")) {
         if(guilds[message.guild.id].queueNames.length < 1) return message.channel.send(`**:x: Nothing playing in this server**`)
