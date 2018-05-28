11ad73ac1421eea92f26ac51e5c64622b5d7a7dd
Abady <gamersspeaks@gmail.com>
2018-05-28 06:41:16 +0300

    try

--- music.js
+++ music.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const ytdl = require("ytdl-core");
-const request = reqduire("request");
+const request = require("request");
 const fs = require("fs");
 const getYouTubeID = require("get-youtube-id");
 const fetchVideoInfo = require("youtube-info");
