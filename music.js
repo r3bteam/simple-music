c56177052573ce96b3da66612caf71c87e7ff33f
Abady <gamersspeaks@gmail.com>
2018-05-30 04:42:31 +0300

    test

--- music.js
+++ music.js
@@ -154,8 +154,8 @@ else if (message.content.startsWith(prefix + 'vol')) {
     if(isNaN(args)) return message.channel.send(`**:x: Volume must be a number -_-**`)
     if (args > 150) return message.reply('**:headphones: For some health reasons the max vol you can use is ``150``, kthx**');
     if (args < 1) return message.reply("**:headphones: you can set volume from ``1`` to ``150``**");
-    guilds[message.guild.id].dispatcher.setVolume(Math.ceil(1 * args / 50))
-    message.channel.send(`**:loud_sound: Volume:** ${args}`);
+    guilds[message.guild.id].dispatcher.setVolume(1 * args / 50)
+    message.channel.send(`**:loud_sound: Volume:** ${guilds[message.guild.id].dispatcher.volume*50}`);
 }
 
 else if (mess.startsWith(prefix + 'pause')) {
