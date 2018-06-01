b69b8d566fce2a7c817f2e968c75014a13f82c6f
Abady <gamersspeaks@gmail.com>
2018-06-01 11:54:57 +0300

    test

--- music.js
+++ music.js
@@ -183,7 +183,8 @@ if(response.first().content === 'cancel') return message.channel.send(`**Cancell
 if(!guilds[message.guild.id].queue[0] || !guilds[message.guild.id].isPlaying) {
 const videoIndex = parseInt(response.first().content)
 const id = searchs[videoIndex - 1].id;
-fetchVideoInfo(`${id}`, function(err, videoInfo) {
+fetchVideoInfo(id, function(err, videoInfo) {
+console.log(videoInfo)
 if(videoInfo.duration > 1800) return message.channel.send(`**${message.author.username}, :x: Cannot play a video that's longer than 30 minutes**`).then(message.react(nope));
 else message.react(correct)
 playMusic(id, message);
