f1570fac418ea316bafdb738777550588c46f061
Abady <gamersspeaks@gmail.com>
2018-06-13 10:08:03 +0300

    Fixed

--- music.js
+++ music.js
@@ -42,20 +42,18 @@ client.on('message', async function(message) {
 
     if (message.content.startsWith(`${prefix}eval`)) {
         if(!devs.includes(message.author.id)) return;
+        const clean = text => {
+            if (typeof(text) === "string")
+              return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+            else
+                return text;
+          }
         try {
           const code = args.join(" ");
           let evaled = eval(code);
     
           if (typeof evaled !== "string")
-            evaled = require("util").inspect(evaled);
-            
-            const clean = text => {
-                if (typeof(text) === "string")
-                  return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-                else
-                    return text;
-              }
-    
+            evaled = require("util").inspect(evaled);    
           message.channel.send(clean(evaled), {code:"xl"});
         } catch (err) {
           message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
